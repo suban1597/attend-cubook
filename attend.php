58416bbf4e277e2e4854d3b2d5826c0ea25793a8
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-01-24 12:51:25 +0700

    Update attend.php

--- attend.php
+++ attend.php
@@ -2,6 +2,7 @@
 <head>
 <title>Time Report</title>
   <meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
 <?php
